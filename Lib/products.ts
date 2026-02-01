f433b227100b0c672853d7e7cdb029b7b0ec2980
FlipFab <letsmailoffice@gmail.com>
2026-02-01 21:52:59 +0530

    Add products list with details in products.ts

---
+++ Lib/products.ts
@@ -0,0 +1,13 @@
+const products = [
+  {
+    slug: "tee",
+    title: "Oversized Street Tee",
+    price: 499,
+    original: 999,
+    gender: "unisex",
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+    link: "https://amazon.in"
+  }
+]
+
+export default products
